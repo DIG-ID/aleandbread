698309310f145110f40e558f85cae1134e6e7e02
bankz91 <brunoduarte.91300@gmail.com>
2025-06-11 15:29:59 +0100

    typography

--- template-parts/design-system.php
+++ template-parts/design-system.php
@@ -1,10 +1,10 @@
 <section class="design-system">
 	<div class="theme-container">
+		<h1 class=" font-barlow text-[58px]">Design System</h1>
 		<div class="theme-grid">
 			<div class="col-span-2 md:col-span-3 xl:col-span-4">
-				<h1 class=" font-barlow text-[58px]">Design System</h1>
 				<div class="mt-12">
-					<h2 class="text-xl font-extrabold uppercase tracking-widest text-black mb-6">Colors</h2>
+					<h2 class="text-accent mb-6">Colors</h2>
 					<ul class="space-y-8 text-center">
 						<li class="w-24 h-24 rounded-full bg-dark"></li>
 						<li class="w-24 h-24 rounded-full bg-accent"></li>
@@ -16,6 +16,14 @@
 					</ul>
 				</div>
 			</div>
+			<div class="col-span-2 md:col-span-3 xl:col-span-4">
+				<div class="mt-12">
+					<h2 class="text-accent mb-6">Typography</h2>
+					<h1 class=" text-dark">H1 title</h1>
+					<h2 class=" text-dark">H2 title</h2>
+					<h3 class=" text-dark">H3 title</h3>
+					<h4 class=" text-dark">H4 title</h4>
+				</div>
 		</div>
 	</div>
 </section>
