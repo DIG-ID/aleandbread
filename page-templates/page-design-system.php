2ae701716fd13b33c3865b0499fefdad8948770e
bankz91 <brunoduarte.91300@gmail.com>
2025-06-11 13:19:38 +0100

    app

--- page-templates/page-design-system.php
+++ page-templates/page-design-system.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Home Template
+ * Template Name: Design System
  */
 
 get_header();
@@ -8,8 +8,7 @@ if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
 		do_action( 'before_main_content' );
-			get_template_part( 'template-parts/pages/home/hero' );	
-			get_template_part( 'template-parts/pages/home/mission-and-offerings' );
+			get_template_part( 'template-parts/design-system' );			
 		do_action( 'after_main_content' );
 	endwhile;
 endif;
