5643220432f73b1941a2050faaf71ed4fc953d63
bankz91 <brunoduarte.91300@gmail.com>
2025-06-12 12:33:46 +0100

    icons and buttons done

--- template-parts/design-system.php
+++ template-parts/design-system.php
@@ -40,21 +40,41 @@
 					<h2 class="text-accent mb-6">Buttons</h2>
 					<div id="main-menu">
 						<div class="menu-item">
-					<a>Menu default</a>
-				</div>
+							<a>Menu default</a>
+						</div>
+					</div>
 				</div>
 				<div>
-					<a class="btn btn-primary">Primary Button</a>
-					<p class="mb-6"></p>
-					<a class="btn btn-secondary">Secondary Button</a>
-					<p class="mb-6"></p>
-					<a class="btn btn-big-button">Big Button</a>
-					<p class="mb-6"></p><p class="mb-6"></p>
-					<a class="btn btn-round-button"></a>
-					<p class="mb-6"></p><p class="mb-6">scroll</p>
-					<a class="btn btn-scroll-button"></a>
-</div>
-</div>
-</div>
-</div>
+					<a class="btn btn-primary mb-6">Primary Button</a>
+					<a class="btn btn-secondary mb-6">Secondary Button</a>
+					<a class="btn btn-big-button mb-6">Big Button</a>
+					<a class="btn btn-tertiary mb-6"><span>Buche dein Erlebnisse</span></a>
+					<a class="btn btn-round-button w-[60px] h-[60px] mb-6"></a>
+					<a class="hidden btn btn-scroll-button">scroll</a>  <!-- a corrigir -->
+					<a class="btn btn-small-button mb-6">In den Warenkorb</a>
+				</div>
+			</div>
+			<div class="col-span-2 md:col-span-3 xl:col-span-3">
+				<div class="mt-12">
+					<h2 class="text-accent mb-6">Icons</h2>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/calendar.svg" alt="Calendar" title="Calendar"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/cart.svg" alt="Cart" title="Cart"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/clock.svg" alt="Clock" title="Clock"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/email.svg" alt="Email" title="Email"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/instagram.svg" alt="Instagram" title="Instagram"/>
+					<br>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/location.svg" alt="Location" title="Location"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/user.svg" alt="User" title="User"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/menu-burger.svg" alt="Menu" title="Menu burger"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/phone.svg" alt="Phone" title="Phone	"/>
+
+					<br>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/location.svg" alt="Location" title="Location"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/user.svg" alt="User" title="User"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/menu-burger.svg" alt="Menu" title="Menu burger"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/phone.svg" alt="Phone" title="Phone	"/>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
