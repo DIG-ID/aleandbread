f83ccabfc89f544da67a1e271a57b20847909d11
Daniel-Digid <daniel@dig.id>
2025-06-12 14:16:57 +0100

    up

--- template-parts/design-system.php
+++ template-parts/design-system.php
@@ -1,104 +1,101 @@
-<section class="design-system">
+<section class="design-system pb-16">
 	<div class="theme-container">
-		<h1 class=" font-barlow text-[58px]">Design System</h1>
+		<h1 class="font-barlow text-[58px]">Design System</h1>
 		<div class="theme-grid">
-			<div class="col-span-2 md:col-span-2 xl:col-span-4">
-				<div class="mt-12">
-					<h2 class="text-accent mb-6">Colors</h2>
-					<ul class="space-y-8 text-center">
-						<li class="w-24 h-24 rounded-full bg-dark"></li>
-						<li class="w-24 h-24 rounded-full bg-accent"></li>
-						<li class="w-24 h-24 rounded-full bg-background"></li>
-						<li class="w-24 h-24 rounded-full bg-blockText"></li>
-						<li class="w-24 h-24 rounded-full bg-blockTextLight"></li>
-						<li class="w-24 h-24 rounded-full bg-formFields"></li>
-						<li class="w-12 h-12 rounded-full bg-importantLinks"></li>
-					</ul>
-				</div>
+			<div class="col-span-2 md:col-span-2 xl:col-span-4 mt-12 border-l border-dark px-8">
+				<h2 class="text-accent mb-6">Colors</h2>
+				<ul class="space-y-8 text-center">
+					<li class="w-24 h-24 rounded-full bg-dark"></li>
+					<li class="w-24 h-24 rounded-full bg-accent"></li>
+					<li class="w-24 h-24 rounded-full bg-background"></li>
+					<li class="w-24 h-24 rounded-full bg-blockText"></li>
+					<li class="w-24 h-24 rounded-full bg-blockTextLight"></li>
+					<li class="w-24 h-24 rounded-full bg-formFields"></li>
+					<li class="w-12 h-12 rounded-full bg-importantLinks"></li>
+				</ul>
 			</div>
-			<div class="col-span-2 md:col-span-2 xl:col-span-4">
-				<div class="mt-12">
-					<h2 class="text-accent mb-6">Typography</h2>
-					<h1 class=" text-dark">H1 title</h1>
-					<h2 class=" text-dark">H2 title</h2>
-					<h3 class=" text-dark">H3 title</h3>
-					<h4 class=" text-dark">H4 title</h4>
-				</div>
+			<div class="col-span-2 md:col-span-2 xl:col-span-4 mt-12 border-l border-dark px-8">
+				<h2 class="text-accent mb-6">Typography</h2>
+				<h1 class=" text-dark">H1 title</h1>
+				<h2 class=" text-dark">H2 title</h2>
+				<h3 class=" text-dark">H3 title</h3>
+				<h4 class=" text-dark">H4 title</h4>
 			</div>
-				<div class="col-span-2 md:col-span-2 xl:col-span-4">
-				<div class="mt-12">
-					<h2 class="text-accent mb-6">Paragraphs</h2>
-					<p class="text-dark block-text mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<p class="text-dark block-text-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div>
-				</div>
-	</div>
-	<div class="theme-container">
-		<div class="theme-grid">
-			<div class="col-span-2 md:col-span-3 xl:col-span-3">
-				<div class="mt-12">
+			<div class="col-span-2 md:col-span-2 xl:col-span-4 mt-12 border-l border-dark px-8">
+				<h2 class="text-accent mb-6">Paragraphs</h2>
+				<p class="text-dark block-text mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+					nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+					dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+					laborum.</p>
+				<p class="text-dark block-text-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+					nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+					dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+					sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+					laborum.</p>
+			</div>
+		</div>
+		<div class="theme-container">
+			<div class="theme-grid">
+				<div class="col-span-2 md:col-span-3 xl:col-span-3 mt-12 border-l border-dark px-8">
 					<h2 class="text-accent mb-6">Buttons</h2>
 					<div id="main-menu">
 						<div class="menu-item">
 							<a>Menu default</a>
 						</div>
 					</div>
+					<div>
+						<a class="btn btn-primary mb-6">Primary Button</a>
+						<a class="btn btn-secondary mb-6">Secondary Button</a>
+						<a class="btn btn-big-button mb-6">Big Button</a>
+						<a class="btn btn-tertiary mb-6"><span>Buche dein Erlebnisse</span></a>
+						<a class="btn btn-round-button w-[60px] h-[60px] mb-6"></a>
+						<a class="!hidden btn btn-scroll-button">scroll</a> <!-- a corrigir -->
+						<a class="btn btn-small-button mb-6">In den Warenkorb</a>
+					</div>
 				</div>
-				<div>
-					<a class="btn btn-primary mb-6">Primary Button</a>
-					<a class="btn btn-secondary mb-6">Secondary Button</a>
-					<a class="btn btn-big-button mb-6">Big Button</a>
-					<a class="btn btn-tertiary mb-6"><span>Buche dein Erlebnisse</span></a>
-					<a class="btn btn-round-button w-[60px] h-[60px] mb-6"></a>
-					<a class="hidden btn btn-scroll-button">scroll</a>  <!-- a corrigir -->
-					<a class="btn btn-small-button mb-6">In den Warenkorb</a>
-				</div>
-			</div>
-			<div class="col-span-2 md:col-span-3 xl:col-span-3">
-				<div class="mt-12">
+				<div class="col-span-2 md:col-span-3 xl:col-span-3 mt-12 border-l border-dark px-8">
 					<h2 class="text-accent mb-6">Icons</h2>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/calendar.svg" alt="Calendar" title="Calendar"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/cart.svg" alt="Cart" title="Cart"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/clock.svg" alt="Clock" title="Clock"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/email.svg" alt="Email" title="Email"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/instagram.svg" alt="Instagram" title="Instagram"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/calendar.svg" alt="Calendar" title="Calendar" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/cart.svg" alt="Cart" title="Cart" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/clock.svg" alt="Clock" title="Clock" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/email.svg" alt="Email" title="Email" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/instagram.svg" alt="Instagram" title="Instagram" />
 					<br>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/location.svg" alt="Location" title="Location"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/user.svg" alt="User" title="User"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/menu-burger.svg" alt="Menu" title="Menu burger"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/phone.svg" alt="Phone" title="Phone	"/>
-
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/location.svg" alt="Location" title="Location" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/user.svg" alt="User" title="User" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/menu-burger.svg" alt="Menu" title="Menu burger" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/phone.svg" alt="Phone" title="Phone" />
 					<br>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/icon1.svg" alt="Location" title="Location"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/icon2.svg" alt="User" title="User"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/icon3.svg" alt="Menu" title="Menu burger"/>
-					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/sun-and-birds.svg" alt="Phone" title="Phone"/>
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/icon1.svg" alt="Location" title="Location" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/icon2.svg" alt="User" title="User" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/icon3.svg" alt="Menu" title="Menu burger" />
+					<img class="inline-block" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svgs/sun-and-birds.svg" alt="Phone" title="Phone" />
 				</div>
-			</div>
-			<div class="col-span-2 md:col-span-3 xl:col-span-3">
-				<div class="mt-12">
+				<div class="col-span-2 md:col-span-3 xl:col-span-3 mt-12 border-l border-dark px-8">
 					<h2 class="text-accent mb-6">Logo</h2>
-					<?php 
-						$image = get_field('theme_logo', 'option');
-						$size = 'full'; // (thumbnail, medium, large, full or custom size)
-						$classes = 'W-[200px] h-[57px]'; // Add any custom classes you want
-						if( $image ) {
-							echo wp_get_attachment_image( $image, $size, false, array('class' => $classes) );
-						}?>
-					<?php 
-						$image = get_field('theme_logo', 'option');
-						$size = 'full'; // (thumbnail, medium, large, full or custom size)
-						if( $image ) {
-							echo wp_get_attachment_image( $image, $size );
-						}?>
-					<?php 
-						$image = get_field('theme_logo', 'option');
-						$size = 'full'; // (thumbnail, medium, large, full or custom size)
-						if( $image ) {
-							echo wp_get_attachment_image( $image, $size );
-						}?>
+					<?php
+					$image = get_field('theme_logo', 'option');
+					$size = 'full'; // (thumbnail, medium, large, full or custom size)
+					$classes = 'W-[200px] h-[57px]'; // Add any custom classes you want
+					if ($image) {
+						echo wp_get_attachment_image($image, $size, false, array('class' => $classes));
+					} ?>
+					<?php
+					$image = get_field('theme_logo', 'option');
+					$size = 'full'; // (thumbnail, medium, large, full or custom size)
+					if ($image) {
+						echo wp_get_attachment_image($image, $size);
+					} ?>
+					<?php
+					$image = get_field('theme_logo', 'option');
+					$size = 'full'; // (thumbnail, medium, large, full or custom size)
+					if ($image) {
+						echo wp_get_attachment_image($image, $size);
+					} ?>
 				</div>
 			</div>
 		</div>
-	</div>
 </section>
