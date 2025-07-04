6ed37d301794556f4381f5accb69b987cb49b7ef
Daniel-Digid <daniel@dig.id>
2025-07-04 15:14:09 +0100

    d

--- inc/theme-woocommerce.php
+++ inc/theme-woocommerce.php
@@ -1,19 +1,38 @@
 <?php
 
-// Remove default WooCommerce wrappers and add custom ones
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+// Remove default WooCommerce wrappers and breadcrumb hook
+remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
+remove_action('woocommerce_shop_loop_header', 'woocommerce_product_taxonomy_archive_header', 10);
 
+// Add custom wrappers
 add_action('woocommerce_before_main_content', 'aleandbread_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'aleandbread_wrapper_end', 10);
 
+// Custom wrapper start
 function aleandbread_wrapper_start() {
-	echo '<main id="main-content" class="main-content overflow-hidden mt-auto pt-[106px]">';
+    echo '<main id="main-content" class="main-content overflow-hidden mt-auto pt-[106px]">';
+    echo '<div class="theme-container pb-[55px] md:pb-[92px] xl:pb-[192px] pt-[152px] md:pt-[194px] xl:pt-[170px]">';
+
+    // Breadcrumbs now live here, *inside* the container
+    if (function_exists('woocommerce_breadcrumb')) {
+        woocommerce_breadcrumb(array(
+            'delimiter'   => ' &raquo; ',
+            'wrap_before' => '<div class="theme-grid"><div class="col-start-1 col-span-1 md:col-span-3 xl:col-start-2 xl:col-span-2 pb-[30px] md:pb-[56px] xl:pb-[58px] w-full"><nav class="woocommerce-breadcrumb">',
+            'wrap_after'  => '</nav></div></div>',
+            'before'      => '',
+            'after'       => '',
+        ));
+    }
 }
 
+// Custom wrapper end
 function aleandbread_wrapper_end() {
-	echo '</main>';
+    echo '</div></main>';
 }
+
+
 
 /**
  * This function closes the main content.
@@ -23,11 +42,11 @@ function aleandbread_shop_categories() {
 	$shop_cats = get_field( 'shop_page_categories', 'option' );
 	if ( $shop_cats ) :
 		?>
-		<ul class="shop-categories grid grid-cols-3 gap-x-5">
+		<ul class="shop-categories theme-grid">
 			<?php
 			foreach( $shop_cats as $shop_cat ) :
 			?>
-				<li class="card-category">
+				<li class="card-category col-span-2 md:col-span-3 xl:col-span-4">
 					<a href="<?php echo esc_url( get_term_link( $shop_cat ) ); ?>">
 						<div class="card-category--content">
 							<span class="overlay"></span>
--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -27,66 +27,97 @@ get_header( 'shop' );
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
 do_action( 'woocommerce_before_main_content' );
+?>
+<section class="shop-intro relative overflow-hidden">
+	<div class="theme-grid pb-14 xl:pb-60">
+		<div class="col-start-1 col-span-2 md:col-span-5 xl:col-start-2 xl:col-span-4">
+			<h1 class="text-dark"><?php echo get_field('intro_title', get_option('woocommerce_shop_page_id')); ?></h1>
+		</div> 
 
-/**
- * Hook: woocommerce_shop_loop_header.
- *
- * @since 8.6.0
- *
- * @hooked woocommerce_product_taxonomy_archive_header - 10
- */
-do_action( 'woocommerce_shop_loop_header' );
+		<div class="col-start-1 col-span-2 md:col-span-4 xl:col-start-8 xl:col-span-4 pt-[48px] md:pt-[56px] xl:pt-0">
+			<p class="text-dark block-text w-[342px] md:w-[438px] xl:w-full">
+			<?php 
+			echo get_field('intro_description', get_option('woocommerce_shop_page_id')); 
+			?>
+			</p> 
+		</div> 
+	</div>
+	<div class="theme-grid">
+		<div class="col-span-2 md:col-span-6 xl:col-span-12">
+		<?php
+		/**
+		 * Hook: woocommerce_archive_description.
+		 *
+		 * @hooked woocommerce_taxonomy_archive_description - 10
+		 * @hooked woocommerce_product_archive_description - 10
+		 */
+		do_action( 'woocommerce_archive_description' );
+		?>
+		<?php
+		/**
+		 * Hook: woocommerce_shop_loop_header.
+		 *
+		 * @since 8.6.0
+		 *
+		 * @hooked woocommerce_product_taxonomy_archive_header - 10
+		 */
+		do_action( 'woocommerce_shop_loop_header' );
 
-if ( woocommerce_product_loop() ) {
-
-	/**
-	 * Hook: aleandbread_shop_categories.
-	 * 
-	 * @hooked aleandbread_shop_categories - 10
-	 */
-		do_action( 'shop_categories' );
-
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked woocommerce_output_all_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action( 'woocommerce_before_shop_loop' );
-
-	woocommerce_product_loop_start();
-
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
-			the_post();
+		if ( woocommerce_product_loop() ) {
 
 			/**
-			 * Hook: woocommerce_shop_loop.
+			 * Hook: aleandbread_shop_categories.
+			 * 
+			 * @hooked aleandbread_shop_categories - 10
 			 */
-			do_action( 'woocommerce_shop_loop' );
+				do_action( 'shop_categories' );
 
-			wc_get_template_part( 'content', 'product' );
+			/**
+			 * Hook: woocommerce_before_shop_loop.
+			 *
+			 * @hooked woocommerce_output_all_notices - 10
+			 * @hooked woocommerce_result_count - 20
+			 * @hooked woocommerce_catalog_ordering - 30
+			 */
+			do_action( 'woocommerce_before_shop_loop' );
+
+			woocommerce_product_loop_start();
+
+			if ( wc_get_loop_prop( 'total' ) ) {
+				while ( have_posts() ) {
+					the_post();
+
+					/**
+					 * Hook: woocommerce_shop_loop.
+					 */
+					do_action( 'woocommerce_shop_loop' );
+
+					wc_get_template_part( 'content', 'product' );
+				}
+			}
+
+			woocommerce_product_loop_end();
+
+			/**
+			 * Hook: woocommerce_after_shop_loop.
+			 *
+			 * @hooked woocommerce_pagination - 10
+			 */
+			do_action( 'woocommerce_after_shop_loop' );
+		} else {
+			/**
+			 * Hook: woocommerce_no_products_found.
+			 *
+			 * @hooked wc_no_products_found - 10
+			 */
+			do_action( 'woocommerce_no_products_found' );
 		}
-	}
+		?>
+		</div>
+	</div>
+</section>
 
-	woocommerce_product_loop_end();
-
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
-} else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
-}
-
+<?php
 /**
  * Hook: woocommerce_after_main_content.
  *
