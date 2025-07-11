a23dcccf73617a2c0cb16f688a49de227c0f43e6
bankz91 <brunoduarte.91300@gmail.com>
2025-07-11 19:24:34 +0100

    1

--- template-parts/pages/about/our-values.php
+++ template-parts/pages/about/our-values.php
@@ -10,8 +10,8 @@
                         echo wp_get_attachment_image( $image, $size, false, array('class' => $classes) );
                     }?> 
             </div>
-                <div class="col-start-1 md:col-start-1 xl:col-start-8 col-span-2 md:col-span-5 xl:col-span-4 pt-[88px] md:pt-[72px] order-2 xl:order-2 md:w-[560px] xl:w-full">
-                    <p class="over-title text-accent pt-[56px] "><?php echo get_field('unsere_werte_over_title'); ?></p>
+                <div class="col-start-1 md:col-start-1 xl:col-start-8 col-span-2 md:col-span-5 xl:col-span-4 order-2 xl:order-2 md:w-[560px] xl:w-full">
+                    <p class="over-title text-accent pt-[88px] md:pt-[72px] "><?php echo get_field('unsere_werte_over_title'); ?></p>
                     <h1 class="text-blockTextLight self-stretch pt-[56px]"><?php echo get_field('unsere_werte_title'); ?></h1>
                     <h3 class="text-blockTextLight block-text-bold pt-[56px]"><?php echo get_field('unsere_werte_sub_title'); ?></h3>
                     <p class="text-blockTextLight block-text pt-[30px]"><?php echo get_field('unsere_werte_description'); ?></p>
