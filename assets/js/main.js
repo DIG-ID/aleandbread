55ed6b4b1cffaf7291361f71e1cf8c62a25de4f4
bankz91 <brunoduarte.91300@gmail.com>
2025-08-21 10:52:11 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 import "./gsap";
 import './navigation';
 import './swiper';
+<<<<<<< Updated upstream
 import './woocommerce-cart';
 import './woocommerce';
 import './woocommerce';
+import './woocommerce';
+=======
+import './woocommerce';
+>>>>>>> Stashed changes
