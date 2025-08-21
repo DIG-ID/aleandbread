fa338e788ecd4327f5f5a2cbe3ca0a26c51883b8
bankz91 <brunoduarte.91300@gmail.com>
2025-08-21 10:53:04 +0100

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
 import "./gsap";
 import './navigation';
 import './swiper';
-<<<<<<< Updated upstream
 import './woocommerce-cart';
 import './woocommerce';
 import './woocommerce';
 import './woocommerce';
-=======
 import './woocommerce';
->>>>>>> Stashed changes
+import './woocommerce';
