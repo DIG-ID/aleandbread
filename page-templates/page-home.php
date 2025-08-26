4e4baeaf3664344c73d57404199b52b26182a399
.peter <pedro@dig.id>
2025-08-26 17:09:02 +0100

    added best sellers to home page

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -8,10 +8,12 @@ if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
 		do_action( 'before_main_content' );
-			get_template_part( 'template-parts/pages/home/hero' );	
+			get_template_part( 'template-parts/pages/home/hero' );
 			get_template_part( 'template-parts/pages/home/mission-and-philosophy' );
 			get_template_part( 'template-parts/pages/home/distellerie' );
 			get_template_part( 'template-parts/pages/home/vineyard' );
+			get_template_part( 'template-parts/pages/home/best-sellers' );
+			get_template_part( 'template-parts/pages/home/experiences' );
 			get_template_part( 'template-parts/modules/events' );
 			get_template_part( 'template-parts/pages/home/awards' );
 			get_template_part( 'template-parts/pages/home/newsletter' );
