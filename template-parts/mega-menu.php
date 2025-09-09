7c630d3e81734d94353587a54089942bee0e9244
Daniel-Digid <daniel@dig.id>
2025-09-09 15:40:16 +0100

    menu

--- template-parts/mega-menu.php
+++ template-parts/mega-menu.php
@@ -1,6 +1,6 @@
-<div class="mega-menu-wrapper bg-dark absolute top-[82px] md:top-[102px] xl:top-[104px] left-0 bottom-0 z-30 w-full h-0 overflow-scroll xl:overflow-hidden transition-all duration-500 ease-in-out xl:flex xl:justify-center xl:items-center">
+<div class="mega-menu-wrapper bg-dark absolute top-[82px] md:top-[102px] xl:top-[104px] left-0 bottom-0 z-30 w-full h-0 overflow-scroll xl:overflow-hidden transition-all duration-500 ease-in-out xl:flex xl:justify-center">
   <div class="theme-container">
-    <div class="mega-menu-content theme-grid gap-y-10 pt-20 md:pt-0 pb-20 md:pb-0 px-[10%]">
+    <div class="mega-menu-content theme-grid gap-y-10 pt-20 md:pt-12 xl:pt-40 pb-20 md:pb-0 px-[10%]">
       <!-- LEFT COLUMN -->
       <div class="col-span-2 md:col-span-6 xl:col-span-4 xl:col-start-3">
         <h2 class="text-[#CBCBCB]">
@@ -32,7 +32,7 @@
       </div>
 
 <!-- RIGHT COLUMN (account section, with standalone divider) -->
-<div class="col-span-2 md:col-span-6 xl:col-span-4 xl:col-start-9 flex">
+<div class="col-span-2 md:col-span-6 xl:col-span-4 xl:col-start-9 flex -ml-5 xl:ml-0">
   
   <!-- Divider -->
   <div class="block mt-[10px] md:mt-[19px] w-[3px] h-[71px] md:h-[106px] bg-accent"></div>
