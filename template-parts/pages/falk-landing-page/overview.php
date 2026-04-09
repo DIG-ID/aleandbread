dfbf0fbd456d19bab55fb35b8303312e8f172d68
bankz91 <brunoduarte.91300@gmail.com>
2026-04-09 13:11:46 +0100

    1

--- template-parts/pages/falk-landing-page/overview.php
+++ template-parts/pages/falk-landing-page/overview.php
@@ -7,7 +7,7 @@
 					<?php echo wp_kses_post( get_field( 'overview_title' ) ); ?>
 				</h2>
 			</div>
-            <div class="col-span-2 md:col-start-1 md:col-span-3 xl:col-start-2 xl:col-span-3 max-w-[379px]">
+            <div class="col-span-2 md:col-start-1 md:col-span-3 xl:col-start-2 xl:col-span-3 max-w-[379px] flex flex-col">
                 <h3 class="h2 text-blockText"><?php echo get_field( 'overview_flandria_title' ); ?></h3>
                 <h4 class="h3 text-blockText pt-4"><?php echo get_field( 'overview_flandria_subtitle' ); ?></h4>
                 <p class="text-blockText pb-14 pt-4"><?php echo get_field( 'overview_flandria_description' ); ?></p>
@@ -18,12 +18,12 @@
 						$link_title = $button['title'];
 						$link_target = $button['target'] ? $button['target'] : '_self';
 						?>
-						<a class="btn btn-primary-2 mb-14 xl:mb-0" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+						<a class="btn btn-primary-2 mt-auto mb-14 xl:mb-0" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
 						<?php echo esc_html($link_title); ?>
 						</a>
 					<?php endif; ?>
             </div>
-            <div class="col-span-2 md:col-start-4 md:col-span-3 xl:col-start-5 xl:col-span-3 xl:pl-16">
+            <div class="col-span-2 md:col-start-4 md:col-span-3 xl:col-start-5 xl:col-span-3 xl:pl-8 flex flex-col">
                 <h3 class="h2 text-blockText"><?php echo get_field( 'overview_classic_title' ); ?></h3>
                 <h4 class="h3 text-blockText pt-4"><?php echo get_field( 'overview_classic_subtitle' ); ?></h4>
                 <p class="text-blockText pb-14 pt-4"><?php echo get_field( 'overview_classic_description' ); ?></p>
@@ -34,12 +34,12 @@
 						$link_title = $button['title'];
 						$link_target = $button['target'] ? $button['target'] : '_self';
 						?>
-						<a class="btn btn-primary-2 mb-14 xl:mb-0" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+						<a class="btn btn-primary-2 mt-auto mb-14 xl:mb-0" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
 						<?php echo esc_html($link_title); ?>
 						</a>
 					<?php endif; ?>
             </div>
-            <div class="col-span-2 md:col-span-3 xl:col-start-8 xl:col-span-3 xl:pl-16">
+            <div class="col-span-2 md:col-span-3 xl:col-start-8 xl:col-span-3 xl:pl-20 flex flex-col">
                 <h3 class="h2 text-blockText"><?php echo get_field( 'overview_signature_title' ); ?></h3>
                 <h4 class="h3 text-blockText pt-4"><?php echo get_field( 'overview_signature_subtitle' ); ?></h4>
                 <p class="text-blockText pb-20 pt-4"><?php echo get_field( 'overview_signature_description' ); ?></p>
@@ -50,7 +50,7 @@
 						$link_title = $button['title'];
 						$link_target = $button['target'] ? $button['target'] : '_self';
 						?>
-						<a class="btn btn-primary-2" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+						<a class="btn btn-primary-2 mt-auto" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
 						<?php echo esc_html($link_title); ?>
 						</a>
 					<?php endif; ?>
